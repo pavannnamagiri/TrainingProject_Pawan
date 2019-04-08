103a8aaf61a8e82834cc6459ae1176752c15df5f
Pawan Namagiri <pavann.namagiri@gmail.com>
2019-04-08 16:16:00 +0530

    added Dao.java

--- src/com/tadigital/ecommerce/customer/dao/Dao.java
+++ src/com/tadigital/ecommerce/customer/dao/Dao.java
@@ -1,62 +1,73 @@
 package com.tadigital.ecommerce.customer.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Dao {
 	protected Connection openConnection() {
 		Connection con = null;
+		InputStream reader=null;
+		reader=getClass().getResourceAsStream("db.properties");
+		Properties prop=new Properties();
 		
+
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tp_db", "root", "");
+			prop.load(reader);
+			Class.forName(prop.getProperty("db.driver"));
+			con = DriverManager.getConnection(prop.getProperty("db.con"), prop.getProperty("db.user"),prop.getProperty("db.password") );
 		} catch (ClassNotFoundException cnfe) {
 			cnfe.printStackTrace();
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
-		
+
 		return con;
 	}
-	
+
 	protected Statement openStatement(Connection con) {
 		Statement stmt = null;
-		
+
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
-		
+
 		return stmt;
 	}
-	
+
 	protected void closeResultSet(ResultSet rs) {
 		try {
-			if(rs != null) {
+			if (rs != null) {
 				rs.close();
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	
+
 	protected void closeStatement(Statement stmt) {
 		try {
-			if(stmt != null) {
+			if (stmt != null) {
 				stmt.close();
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	
+
 	protected void closeConnection(Connection con) {
 		try {
-			if(con != null) {
+			if (con != null) {
 				con.close();
 			}
 		} catch (SQLException sqle) {
